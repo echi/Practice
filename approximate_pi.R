cfd1bead363630a2552d8cb9cfac20375272cad4
Eric Chi <ecchi@ncsu.edu>
2019-08-16 22:04:31 -0400

    Adding approximate_pi.R
    This reverts commit 0ef708fcf509abe9d4fdacfe11cf689132987c8e.

---
+++ approximate_pi.R
@@ -0,0 +1,7 @@
+approximate_pi <- function(darts=500) {
+  dx <- runif(darts, -1, 1)
+  dy <- runif(darts, -1, 1)
+  in_circle <- which(dx^2 + dy^2 < 1)
+  pi <- 4 * length(in_circle)/darts
+  return(pi)
+}
--- mynorm.R
+++ mynorm.R
@@ -1,6 +1,3 @@
-#' mynorm computes the norm of a vector
 mynorm <- function(x) {
   return(sqrt(sum(x**2)))
 }
-
-## Lalalala
